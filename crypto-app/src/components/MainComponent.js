f959c8f9bc91a7a5a6aa3c6428f54ce456b78b58
AvdicAdnan <avdic_adnan@hotmail.com>
2022-10-16 14:32:53 +0200

    Every crypto token added with few details

--- crypto-app/src/components/MainComponent.js
+++ crypto-app/src/components/MainComponent.js
@@ -16,20 +16,38 @@ function MainComponent() {
 
   if (!crypto) return null;
 
+  const cryptoCoins = crypto.map((element) => {
+    return (
+      <div className="main-wrapper" key={element.id}>
+        <div className="img-name-wrapper">
+          <img src={element.image} alt="crypto-img" className="crypto-logo" />
+          <h2>{element.name}</h2>
+        </div>
+
+        <p className="current-price">{element.current_price}$</p>
+
+        <div className="price-wrapper">
+          <p className="highest-price">
+            {element.high_24h}$ <span>24h%</span>
+          </p>
+          <p className="lowest-price">
+            {element.low_24h}$ <span>24h%</span>
+          </p>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <h1>Crypto Price Tracker</h1>
-
       <div className="crypto-wrapper">
-        <div className="main-wrapper">
-          <h2>{crypto[0].name}</h2>
-          <p>{crypto[0].high_24h}$</p>
+        <div className="details-wrapper">
+          <p>Name</p>
+          <p>Price</p>
+          <p>Highest price/ Lowest Price</p>
         </div>
-
-        <div className="main-wrapper">
-          <h2>{crypto[1].name}</h2>
-          <p>{crypto[1].high_24h}$</p>
-        </div>
+        {cryptoCoins}
       </div>
     </div>
   );
